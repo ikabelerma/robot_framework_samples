6ce5214921e655b5ded008eedacfd869a823c73f
Ika Belerma <ika.belerma@gmail.com>
2014-10-21 00:54:10 +0800

    Added all samples.
    I know, it's a big commit. I got lazy. Basically these are sample
    robot test cases I can use on my workshops.
    
    Signed-off-by: Ika Belerma <ika.belerma@gmail.com>

---
+++ resources/ExampleLibrary.py
@@ -0,0 +1,9 @@
+class ExampleLibrary:
+
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+
+    def return_hello_world(self):
+        return "Hello World"
+
+    def convert_to_upper_case(self, arg):
+        return arg.upper()
